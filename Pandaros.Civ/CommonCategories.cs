c7acea2f40e57a967641b3002296708327b97f53
NACH0CHEE5E <39394001+NACH0CHEE5E@users.noreply.github.com>
2020-09-04 13:02:25 +1000

    Update CommonCategories.cs

--- Pandaros.Civ/CommonCategories.cs
+++ Pandaros.Civ/CommonCategories.cs
@@ -20,5 +20,21 @@ namespace Pandaros.Civ
         public static string RecruitmentItem = "recruitment";
         public static string Job = "job";
         public static string Food = "food";
+
+        public static List<string> PreHistory = new List<string>
+        {
+            "aa",
+            nameof(TimePeriod.PreHistory)
+        };
+        public static List<string> StoneAge = new List<string>
+        {
+            "ba",
+            nameof(TimePeriod.StoneAge)
+        };
+        public static List<string> BronzeAge = new List<string>
+        {
+            "ca",
+            nameof(TimePeriod.BronzeAge)
+        };
     }
 }
