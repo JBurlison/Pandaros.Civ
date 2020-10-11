468e4a78d3aaedc2abd33ef10c4a683afd375a8a
Jim Burlison <JBurlison@Gmail.com>
2020-10-11 00:18:47 -0400

    Fix Berry bush

---
+++ Pandaros.Civ/PlayerBehavior/BerryBushDestory.cs
@@ -0,0 +1,25 @@
+﻿using ModLoaderInterfaces;
+using Pandaros.API;
+using Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pandaros.Civ.PlayerBehavior
+{
+    public class BerryBushDestory : IOnTryChangeBlock
+    {
+        public void OnTryChangeBlock(ModLoader.OnTryChangeBlockData tryChangeBlockData)
+        {
+            if (tryChangeBlockData.PlayerClickedData != null && tryChangeBlockData.PlayerClickedData.HitType != Shared.PlayerClickedData.EHitType.Block)
+                return;
+
+            if (tryChangeBlockData.TypeOld.Name == ColonyBuiltIn.ItemTypes.BERRYBUSH)
+            {
+                tryChangeBlockData.InventoryItemResults.Add(new InventoryItem(ColonyBuiltIn.ItemTypes.BERRYBUSH.Id, Pipliz.Random.Next(30, 51)));
+            }
+        }
+    }
+}
